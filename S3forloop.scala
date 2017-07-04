5cea5f15895bd82c3b530afdd72d8ec9aeb1e06b
sumitganguly85 <sumitganguly85@gmail.com>
2017-07-04 17:29:13 +0530

    Create S3forloop.scala

---
+++ S3forloop.scala
@@ -0,0 +1,25 @@
+package com.cloudera.student
+
+object S3forloop {
+  def main(args: Array[String])
+  {
+    var a=0
+    val another = 123
+    
+    for(a <- 5 to 1 by -1)
+    //for(a <- 5 to 1 by -2)
+    //for(a <- 1 to 5 by 2)
+    for(a <- 1 to 5)
+    {
+      println("This is the value of a: " + a)
+    }
+    
+    var array1 = Array(1.9,2.9,3.4,3.5)
+    for(x <- array1)
+    {
+      println(x)
+      println("another result " + x*x)
+      
+    }
+  }
+}
