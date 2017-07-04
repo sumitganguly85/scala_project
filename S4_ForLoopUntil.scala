3c8667a3c4922c607b4a99e6884beb607147ec27
sumitganguly85 <sumitganguly85@gmail.com>
2017-07-04 17:32:57 +0530

    Create S4_ForLoopUntil.scala

---
+++ S4_ForLoopUntil.scala
@@ -0,0 +1,13 @@
+package com.cloudera.student
+
+object S4_ForLoopUntil {
+  def main(args: Array[String])
+  {
+    for(i <- 1 until 8)
+    {
+      println("This is the value of a (using until): " + i);
+    }
+    
+  }
+  
+}
