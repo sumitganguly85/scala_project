f9a25a15a396b4698452ec333151dd7102d8ad36
sumitganguly85 <sumitganguly85@gmail.com>
2017-07-07 17:53:34 +0530

    Create S1_Class_Example.scala

---
+++ S1_Class_Example.scala
@@ -0,0 +1,14 @@
+/**
+ * @author 444109
+ */
+class Duck {
+  
+  var size =1
+}
+
+object DuckDemo extends App
+{
+  var f = new Duck
+  f.size =10
+  println("The size of variable is " + f.size)
+}
